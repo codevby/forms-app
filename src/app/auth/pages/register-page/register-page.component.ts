1b4b8e9ef4b60b85f90099e14ae5ebcf09636dd9
Leonardo <leonardogarcarr23@gmail.com>
2024-09-21 00:25:46 -0400

    name and email validation

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
-import { cantBeStrider } from '../../../shared/validators/validators';
+import * as customValidators from '../../../shared/validators/validators';
 
 @Component({
   selector: 'app-register-page',
@@ -18,9 +18,9 @@ export class RegisterPageComponent {
   private fb = inject(FormBuilder);
 
   public myForm: FormGroup = this.fb.group({
-    name:  [ '', [ Validators.required ]],
-    email: [ '', [ Validators.required ]],
-    username:  [ '', [ Validators.required, cantBeStrider ]],
+    name:  [ '', [ Validators.required, Validators.pattern( customValidators.firstNameAndLastnamePattern ) ]],
+    email: [ '', [ Validators.required, Validators.pattern( customValidators.emailPattern ) ]],
+    username:  [ '', [ Validators.required, customValidators.cantBeStrider ]],
     password:  [ '', [ Validators.required, Validators.minLength(6) ]],
     password2: [ '', [ Validators.required, Validators.minLength(6) ]]
   })
