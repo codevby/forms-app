d58244ec837dab094e63c02bc600d9f5ece518cc
Leonardo <leonardogarcarr23@gmail.com>
2024-09-24 01:59:12 -0400

    other async validator to email

--- src/app/shared/validators/email-validator.service.ts
+++ src/app/shared/validators/email-validator.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AbstractControl, AsyncValidator, ValidationErrors } from '@angular/forms';
-import { Observable, of } from 'rxjs';
+import { delay, Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +11,35 @@ export class EmailValidatorService implements AsyncValidator {
 
     const email = control.value;
 
-    return of({
-      emailTaken: true
-    })
+    const httpCallObservable = new Observable<ValidationErrors | null>( ( subscriber ) => {
+      console.log({ email });
+      if ( email === 'fernando@google.com' ) {
+        subscriber.next({ emailTaken: true });
+        subscriber.complete();
+        //return;
+      }
+
+      subscriber.next(null);
+      subscriber.complete();
+
+    }).pipe(
+      delay( 3000 )
+    );
+
+    return httpCallObservable;
+
+    // return this.http.get(`http://localhost3000/user?q=${ email }`)
 
   }
+
+  // validate(control: AbstractControl): Observable<ValidationErrors | null> {
+
+  //   const email = control.value;
+
+  //   return of({
+  //     emailTaken: true
+  //   })
+
+  // }
 
 }
